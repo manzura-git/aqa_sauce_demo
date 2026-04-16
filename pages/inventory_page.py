c7b88e37652b54e215cf97dccac648579257b639
manzura-git <manzura.qayumova@alif.tj>
2026-04-16 09:59:35 +0500

    Initial commit

---
+++ pages/inventory_page.py
@@ -0,0 +1,32 @@
+from playwright.sync_api import expect
+
+from config.products import BACKPACK
+from pages.base_page import BasePage
+
+
+class InventoryPage(BasePage):
+
+    def __init__(self, page):
+        super().__init__(page)
+        self.backpack1 = self.page.get_by_text(BACKPACK)
+        self.price = self.page.locator(f"//*[text()='{BACKPACK}']/../../..//*[@class='inventory_item_price']")
+        self.btn_add_to_card = self.page.locator(f"//*[text()='{BACKPACK}']/../../..//button")
+        # self.price = self.page.locator(".inventory_item_price")
+        # self.loc_price = ".inventory_item_price"
+        self.loc_price = "../../*[@class='inventory_item_price']"
+
+    def check_backpack1_visible(self):
+        expect(self.backpack1).to_be_visible()
+
+    def get_backpack1_price(self) -> str:
+        price_ = self.price.text_content()
+        return price_  # .replace("$", "")
+        # return self.backpack1.locator(self.loc_price).text_content()
+        # return self.backpack1.locator("../").locator(self.loc_price).text_content()
+        # return self.backpack1.text_content()
+
+    def check_is_price(self):
+        assert self.get_backpack1_price().startswith("$")
+
+    def click_btn_add_to_cart(self):
+        self.btn_add_to_card.click()
